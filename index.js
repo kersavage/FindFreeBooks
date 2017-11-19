2bedd5e536a2562f1ec45dce234bac424c81208a
kersavage <kylor_k@hotmail.com>
2017-11-18 20:08:28 -0700

    milestone1

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+//var pg = require("pg"); // This is the postgres database connection module.
+//const connectionString = "postgres://postgres:1234@localhost/webnotes";
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var express = require('express');
+var app = express();
+var url = require('url');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+app.get('/login', function(request, response) {
+	login(request, response);
+});
+
+function login(request, response) {
+	var requestUrl = url.parse(request.url, true);
+
+	console.log("Login info: " + JSON.stringify(requestUrl.query));
+
+	// TODO: Here we should check to make sure we have all the correct parameters
+
+	var name = requestUrl.query.uname;
+	var password = requestUrl.query.psw;
+
+	var credentials = {username: name};
+
+	response.render('pages/webnotes', credentials);
+
+}
+
