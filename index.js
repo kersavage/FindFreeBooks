1d708ff889533b5589bb48105b65de8043400efe
kersavage <kylor_k@hotmail.com>
2017-11-29 00:51:05 -0700

    removed request

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var express = require('express');
 var app = express();
 var url = require('url');
 var https = require('https');
-var request = require('request');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -17,16 +16,6 @@ app.set('view engine', 'ejs');
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
-
-    	/*request('https://www.googleapis.com/books/v1/volumes?q=flowers&filter=free-ebooks&key=AIzaSyA-8ljiTTPG7KeuylwBhzjwL3X8J4YM98o', function(error, response, body){
-		 	if (!error && response.statusCode == 200) {
-    			console.log("Here is the body: " + body) } // Print the google web page.
-
-    		if(response.statusCode != 200) {
-    			console.log("There was an error: " + error);
-    	}
-});*/
-
 });
 
 app.get('/login', function(request, response) {
@@ -46,5 +35,4 @@ function login(request, response) {
 	var credentials = {username: name};
 
 	response.render('pages/webnotes', credentials);
-
 }
